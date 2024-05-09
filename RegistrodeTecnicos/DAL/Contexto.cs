4ea81980618523abd466cb3d598d2d2cc9c17856
Cape23180909 <samilgamer@outlook.com>
2024-05-09 01:24:42 -0400

    Modificado

---
+++ RegistrodeTecnicos/DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistrodeTecnicos.Models;
+
+namespace RegistrodeTecnicos.Pages.DAL
+{
+    public class Contexto : DbContext
+    {
+
+        public Contexto(DbContextOptions<Contexto> options)
+            : base(options) { }
+
+        public DbSet<Tecnicos> Tecnicos {  get; set; } 
+    }
+}
