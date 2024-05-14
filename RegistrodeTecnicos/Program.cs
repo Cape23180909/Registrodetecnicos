e928aafa457dbeafaf9805f8350225dd804798ec
Cape23180909 <samilgamer@outlook.com>
2024-05-13 23:36:49 -0400

    Estaba quitando los corchetes del namespace

--- RegistrodeTecnicos/Program.cs
+++ RegistrodeTecnicos/Program.cs
@@ -3,43 +3,41 @@ using RegistrodeTecnicos.Components;
 using RegistrodeTecnicos.Pages.DAL;
 using RegistrodeTecnicos.Servies;
 
-namespace RegistrodeTecnicos
+namespace RegistrodeTecnicos;
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-      {
-            var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
 
-            // Agregamos el contexto al builder con el ConStr// Agregamos el contexto al builder con el ConStr //Inyectar el servies  //Obtenemos el ConStr para usarlo en el contexto
-            var ConStr = builder.Configuration.GetConnectionString("ConStr");
-            builder.Services.AddDbContext<Contexto>(Options => Options.UseSqlite(ConStr));
-            builder.Services.AddScoped<TecnicoServies>();
-            builder.Services.AddBlazorBootstrap();
+        // Agregamos el contexto al builder con el ConStr// Agregamos el contexto al builder con el ConStr //Inyectar el servies  //Obtenemos el ConStr para usarlo en el contexto
+        var ConStr = builder.Configuration.GetConnectionString("ConStr");
+        builder.Services.AddDbContext<Contexto>(Options => Options.UseSqlite(ConStr));
+        builder.Services.AddScoped<TecnicoServies>();
+        builder.Services.AddBlazorBootstrap();
 
-            var app = builder.Build();
+        var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseStaticFiles();
-            app.UseAntiforgery();
-
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode();
-
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
     }
 }
