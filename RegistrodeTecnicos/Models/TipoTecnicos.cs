ac4a32ef76cef4f945d70cae9adbe1218e3fe699
Cape23180909 <samilgamer@outlook.com>
2024-05-16 14:15:58 -0400

    Cambiando la variable p por t ya que estoy trabajando un registro de tecnicos no de prioridad

---
+++ RegistrodeTecnicos/Models/TipoTecnicos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistrodeTecnicos.Models
+{
+    public class TipoTecnicos
+    {
+        [Key]
+
+        public int TecnicoId { get; set; }
+        [Required(ErrorMessage = "El campo no esta lleno del tipo tecnico")]
+        public string Descripcion { get; set; }
+    }
+}
