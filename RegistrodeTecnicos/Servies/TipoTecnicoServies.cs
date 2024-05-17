5ddc1385face8756366fca1fdea269732a4ea737
Cape23180909 <samilgamer@outlook.com>
2024-05-17 01:58:59 -0400

    Colocando el boton Guardar correctamente y asegurado el servies que este bien

--- RegistrodeTecnicos/Servies/TipoTecnicoServies.cs
+++ RegistrodeTecnicos/Servies/TipoTecnicoServies.cs
@@ -2,76 +2,34 @@
 using RegistrodeTecnicos.Models;
 using RegistrodeTecnicos.Pages.DAL;
 using System.Linq.Expressions;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace RegistrodeTecnicos.Servies
 {
     public class TipoTecnicoServies
     {
-        private readonly Contexto Contexto;
+        private readonly Contexto _context;
 
-        public TipoTecnicoServies(Contexto contexto)
+        public TipoTecnicoServies(Contexto context)
         {
-            Contexto = contexto;
+            _context = context;
         }
 
-        // Método Existe
-        public async Task<bool> Existe(int tipoTecnicoId)
+        public async Task<bool> Guardar(TipoTecnicos tipoTecnicos)
         {
-            return await Contexto.TipoTecnicos.AnyAsync(t => t.TipoId == tipoTecnicoId);
-        }
-
-        // Método Insertar
-        private async Task<bool> Insertar(TipoTecnicos tipoTecnico)
-        {
-            Contexto.TipoTecnicos.Add(tipoTecnico);
-            return await Contexto.SaveChangesAsync() > 0;
-        }
-
-        // Método Modificar
-        private async Task<bool> Modificar(TipoTecnicos tipoTecnico)
-        {
-            Contexto.TipoTecnicos.Update(tipoTecnico);
-            var modificado = await Contexto.SaveChangesAsync() > 0;
-            Contexto.Entry(tipoTecnico).State = EntityState.Detached;
-            return modificado;
-        }
-
-        // Método Guardar
-        public async Task<bool> Guardar(TipoTecnicos tipoTecnico)
-        {
-            if (!await Existe(tipoTecnico.TipoId))
-                return await Insertar(tipoTecnico);
-            else
-                return await Modificar(tipoTecnico);
-        }
-
-        // Método Eliminar
-        public async Task<bool> Eliminar(int id)
-        {
-            var tipoTecnico = await Contexto.TipoTecnicos.FindAsync(id);
-            if (tipoTecnico != null)
+            if (tipoTecnicos.TipoId == 0)
             {
-                Contexto.TipoTecnicos.Remove(tipoTecnico);
-                return await Contexto.SaveChangesAsync() > 0;
+                _context.TipoTecnicos.Add(tipoTecnicos);
             }
-            return false;
+            else
+            {
+                _context.TipoTecnicos.Update(tipoTecnicos);
+            }
+            return await _context.SaveChangesAsync() > 0;
         }
 
-        // Método Buscar
-        public async Task<TipoTecnicos?> Buscar(int id)
-        {
-            return await Contexto.TipoTecnicos
-                .AsNoTracking()
-                .FirstOrDefaultAsync(t => t.TipoId == id);
-        }
-
-        // Método Listar
         public async Task<List<TipoTecnicos>> Listar(Expression<Func<TipoTecnicos, bool>> criterio)
         {
-            return await Contexto.TipoTecnicos
+            return await _context.TipoTecnicos
                 .AsNoTracking()
                 .Where(criterio)
                 .ToListAsync();
