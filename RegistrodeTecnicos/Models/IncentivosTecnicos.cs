2833848923efc26d26cdc1653def796ccb60200a
Cape23180909 <samilgamer@outlook.com>
2024-05-25 01:30:36 -0400

    Sustituyendo el tipo de dato string por date en la propiedad fecha y la propiedad Monto de long a decimal el tipo de dato

--- RegistrodeTecnicos/Models/IncentivosTecnicos.cs
+++ RegistrodeTecnicos/Models/IncentivosTecnicos.cs
@@ -3,17 +3,13 @@
 namespace RegistrodeTecnicos.Models
 {
     public class IncentivosTecnicos
-
     {
         [Key]
-
         public int IncentivoId { get; set; }
-        public string Fecha { get; set; }
+        public DateTime Fecha { get; set; } 
         public int TecnicoId { get; set; }
         public string Descripcion { get; set; }
         public int CantidadServicios { get; set; }
-        public long Monto { get; set; }
-
-
+        public decimal Monto { get; set; } 
     }
-    }
+}
