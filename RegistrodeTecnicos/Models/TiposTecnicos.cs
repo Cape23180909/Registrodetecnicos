8552acb0f1bdb996d890b6c5586cff144b7c4fa2
Cape23180909 <samilgamer@outlook.com>
2024-05-25 03:46:57 -0400

    Colocando la nueva propiedad "Incentivo"

--- RegistrodeTecnicos/Models/TiposTecnicos.cs
+++ RegistrodeTecnicos/Models/TiposTecnicos.cs
@@ -6,8 +6,9 @@ public class TiposTecnicos
     [Key]
     public int TipoId { get; set; }
     [Required(ErrorMessage = "El campo no esta lleno")]
-
     public string Descripcion { get; set; }
+    [Required(ErrorMessage = "Ingrese un insentivo")]
+    public int Incentivo { get; set; }
 
 
 }
