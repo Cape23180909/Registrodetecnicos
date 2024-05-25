3839a0769a8172a406d6d043983ce75224e75ec4
Cape23180909 <samilgamer@outlook.com>
2024-05-25 04:13:26 -0400

    Indentando el codigo de la clase IncentivosTecnicos

--- RegistrodeTecnicos/Models/IncentivosTecnicos.cs
+++ RegistrodeTecnicos/Models/IncentivosTecnicos.cs
@@ -6,10 +6,10 @@ namespace RegistrodeTecnicos.Models
     {
         [Key]
         public int IncentivoId { get; set; }
-        public DateTime Fecha { get; set; } 
+        public DateTime Fecha { get; set; }
         public int TecnicoId { get; set; }
         public string Descripcion { get; set; }
         public int CantidadServicios { get; set; }
-        public decimal Monto { get; set; } 
+        public decimal Monto { get; set; }
     }
 }
