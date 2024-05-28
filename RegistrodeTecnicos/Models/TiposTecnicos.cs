c4b2f3e8811d12d4ac759d93a338ad90b1668046
Cape23180909 <samilgamer@outlook.com>
2024-05-28 01:04:53 -0400

    Agregando la nueva propiedad llamada Tecnicos

--- RegistrodeTecnicos/Models/TiposTecnicos.cs
+++ RegistrodeTecnicos/Models/TiposTecnicos.cs
@@ -9,6 +9,9 @@ public class TiposTecnicos
     public int TipoId { get; set; }
     [Required(ErrorMessage = "El campo descripcion no esta lleno")]
     public string? Descripcion { get; set; }
-    [Required(ErrorMessage = "El campo incentivo no esta lleno")]
     public decimal? Incentivo { get; set; }
+
+    public Tecnicos? Tecnicos { get; set; }
+
+
 }
