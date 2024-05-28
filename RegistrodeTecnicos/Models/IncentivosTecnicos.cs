f06b96a72720b28869cc71e3e3fabed0af5777ec
Cape23180909 <samilgamer@outlook.com>
2024-05-28 01:03:27 -0400

    Colocando la ForeignKey correcta donde va de mi modelo

--- RegistrodeTecnicos/Models/IncentivosTecnicos.cs
+++ RegistrodeTecnicos/Models/IncentivosTecnicos.cs
@@ -1,22 +1,22 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace RegistrodeTecnicos.Models
-{
+namespace RegistrodeTecnicos.Models;
+
     public class IncentivosTecnicos
     {
         [Key]
         public int IncentivoId { get; set; }
         [Required(ErrorMessage = "El campo fecha no esta lleno")]
         public DateTime Fecha { get; set; }
+        [ForeignKey("Tecnicos")]
         public int TecnicoId { get; set; }
         [Required(ErrorMessage = "El campo descripcion no esta lleno")]
         public string Descripcion { get; set; }
         [Required(ErrorMessage = "El campo cantidad de servicios no esta lleno")]
         public int? CantidadServicios { get; set; }
         [Required(ErrorMessage = "El campo Monto no esta lleno")]
-
-        [ForeignKey("Tecnicos")]
         public decimal? Monto { get; set; }
+
+        public Tecnicos? Tecnicos { get; set; }
     }
-}
